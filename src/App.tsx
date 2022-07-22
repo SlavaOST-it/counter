562c97dffcb25a20b1a55bc712118ae659352b01
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2022-07-22 19:44:38 +0300

    add useEffect and localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {Counter} from "./components/Counter";
 import s from "./Counter.module.css"
@@ -8,20 +8,36 @@ function App() {
     const minValue = 0
     const maxValue = 5
 
-    let [num, setNum] = useState(minValue)
+    let [num, setNum] = useState<number>(minValue)
 
     const [value, setValue] = useState(0)
 
+    useEffect(() => {
+        let valueAsString = localStorage.getItem('counterValue')
+        if (valueAsString) {
+            let newValue = JSON.parse(valueAsString)
+            setNum(newValue)
+        }
+    }, [])
 
+    useEffect(() => {
+        localStorage.setItem('counterValue', JSON.stringify(num))
+    }, [num])
+
+
+    // Логика для Counter
     const incBtn = () => {
         if (num < maxValue) {
             setNum(num + 1)
         }
     }
-
     const resBtn = () => {
         setNum(0)
     }
+
+    // Логика для SettingsCounter
+
+
 
 
     return (
@@ -30,7 +46,9 @@ function App() {
                 value={value}
                 setValue={setValue}
                 minValue={minValue}
-                maxValue={maxValue}/>
+                maxValue={maxValue}
+            />
+
 
 
             <Counter
