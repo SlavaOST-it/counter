07b3654b653a7dfbd69d0c349d3fe17616ac2a22
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2022-07-23 20:28:09 +0300

    add functions: onChangeSetMaxValue, onChangeSetMinValue

--- src/components/SettingsCounter.tsx
+++ src/components/SettingsCounter.tsx
@@ -1,8 +1,61 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
+import s from "../Counter.module.css"
+import {Button} from "./Button";
 
-export const SettingsCounter = () => {
+
+export type SettingsCounterType = {
+    num: number
+    minValue: number
+    maxValue: number
+    setMinValue: (minValue: number) => void
+    setMaxValue: (maxValue: number) => void
+    setBtn: () => void
+    setError: (error: string) => void
+}
+
+export const SettingsCounter = (props: SettingsCounterType) => {
+
+    const onChangeSetMaxValue = (event: ChangeEvent<HTMLInputElement>) => {
+        let a = (event.currentTarget.value)
+        let maxNum = parseInt(a)
+        props.setMaxValue(maxNum)
+        if (maxNum <= props.minValue) {
+            props.setError('Incorrect value!')
+        } else {
+            props.setError('')
+        }
+    }
+
+    const onChangeSetMinValue = (event: ChangeEvent<HTMLInputElement>) => {
+        let a = (event.currentTarget.value)
+        let minNum = parseInt(a)
+        props.setMinValue(minNum)
+        if (minNum >= props.maxValue){
+            props.setError('Incorrect value!')
+        } else {
+            props.setError('')
+        }
+    }
+
+
+    const setCallbackHandler = () => {
+        props.setBtn()
+    }
+
+    // const disableHandler = ()
     return (
-        <div>
+        <div className={s.settingsBody}>
+            <div className={s.inputDiv}>
+                <div className={s.valueDiv}>
+                    <span className={s.valueText}>max value:</span>
+                    <input value={props.maxValue} type={'number'} onChange={onChangeSetMaxValue} className={s.input}/>
+                </div>
+                <div className={s.valueDiv}>
+                    <span className={s.valueText}>start value:</span>
+                    <input value={props.minValue} type={'number'} onChange={onChangeSetMinValue} className={s.input}/>
+                </div>
+            </div>
+            <Button title={'set'} callBack={setCallbackHandler} className={s.btn} disable={false}/>
 
         </div>
     );
