ecec59d84d1fa620906d2322b11420a8f98095b0
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2022-08-28 21:44:45 +0300

    refactoring code

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,43 +1,67 @@
-import React from "react";
+import React, {ChangeEvent, useState} from "react";
 import s from "../Counter.module.css"
 import {Button} from "./Button";
-import {Tablo} from "./Tablo";
+import {Scoreboard} from "./Scoreboard";
+import {Settings} from "./Settings";
+
 
 type CounterType = {
     minValue: number
     maxValue: number
+    error: boolean
+
     num: number,
-    incBtnCallback: (minValue: number) => void
+    incBtnCallback: () => void
     resBtn: () => void
-    error:string
+    onChangeSetMaxValue: (event: ChangeEvent<HTMLInputElement>) => void,
+    onChangeSetMinValue: (event: ChangeEvent<HTMLInputElement>) => void
 }
 
 export const Counter = (props: CounterType) => {
 
-    const incBtnHandler = () => props.incBtnCallback(props.minValue)
+    const [board, setBoard] = useState(true)
 
+    const incBtnHandler = () => props.incBtnCallback()
     const resetBtnHandler = () => props.resBtn()
 
-    const inputTitle = props.error ? props.error : props.num
-    let messageError = <div></div>
+    const changeBoard = () => {
+        setBoard(!board)
+    }
+
     return (
-        <div className={s.body}>
-            <Tablo className={props.num < props.maxValue ? s.tablo : s.numRed}
-                   title={inputTitle}/>
-            <div className={s.btnDiv}>
-                <Button
-                    title={'inc'}
-                    callBack={incBtnHandler}
-                    className={`${s.inc} + ' ' + ${s.btn}`}
-                    disable={props.num === props.maxValue}
-                />
-                <Button
-                    title={'reset'}
-                    callBack={resetBtnHandler}
-                    className={`${s.reset} + ' ' + ${s.btn}`}
-                    disable={props.num === props.minValue}
-                />
+        board ? (
+            <div className={s.body}>
+                <Scoreboard className={props.num < props.maxValue ? s.tablo : s.numRed}
+                            title={props.num}/>
+                <div className={s.btnDiv}>
+                    <Button
+                        title={'inc'}
+                        callBack={incBtnHandler}
+                        className={`${s.inc} + ' ' + ${s.btn}`}
+                        disable={props.num === props.maxValue}
+                    />
+                    <Button
+                        title={'reset'}
+                        callBack={resetBtnHandler}
+                        className={`${s.reset} + ' ' + ${s.btn}`}
+                        disable={props.num === props.minValue}
+                    />
+                    <Button
+                        title={'set'}
+                        callBack={changeBoard}
+                        className={s.btn}
+                        disable={false}/>
+                </div>
             </div>
-        </div>
+        ) : (
+            <Settings
+                maxValue={props.maxValue}
+                minValue={props.minValue}
+                onChangeSetMaxValue={props.onChangeSetMaxValue}
+                onChangeSetMinValue={props.onChangeSetMinValue}
+                changeBoard={changeBoard}
+                error={props.error}
+            />
+        )
     )
 }
