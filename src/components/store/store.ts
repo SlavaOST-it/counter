8dcc65e965bde54bfde5b1053ce2a61ffe2ef7e7
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2022-10-08 13:50:20 +0300

    add store.subscribe and persistedState

--- src/components/store/store.ts
+++ src/components/store/store.ts
@@ -1,10 +1,17 @@
-
 import {combineReducers, createStore} from "redux";
 import {settingsReducer} from "../bll/reducer/settingsReducer";
+import {loadState, saveState} from "../../utils/localstorage-util";
+
+const persistedState = loadState();
 
 const rootReducer = combineReducers({
     settings: settingsReducer
 })
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, persistedState)
+store.subscribe(() => {
+    saveState({
+        settings: store.getState().settings
+    });
+});
 export type AppRootStateType = ReturnType<typeof rootReducer>
