9a091fe32840e78cf1e39b185f8e76c747f0a1e9
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2022-10-08 09:22:11 +0300

    refactoring code

--- src/components/bll/reducer/settingsReducer.ts
+++ src/components/bll/reducer/settingsReducer.ts
@@ -4,9 +4,8 @@ type IncrementValueAT = ReturnType<typeof incrementValueActionCreator>
 type ResetValueAT = ReturnType<typeof resetValueActionCreator>
 type AddErrorAT = ReturnType<typeof addErrorActionCreator>
 
+
 type ActionType = ChangeMaxValueAT | ChangeMinValueAT | IncrementValueAT | ResetValueAT | AddErrorAT
-
-
 type InitialStateType = typeof initialState
 
 const initialState = {
@@ -18,71 +17,36 @@ const initialState = {
 
 export const settingsReducer = (state: InitialStateType = initialState, action: ActionType): InitialStateType => {
     switch (action.type) {
-        case "SET-MAX-VALUE": {
+        case "SET-MAX-VALUE":
             return {
                 ...state, maxValue: action.maxValue
             }
-        }
-
-        case "SET-MIN-VALUE": {
+        case "SET-MIN-VALUE":
             return {
                 ...state,
                 minValue: action.minValue,
                 num: action.minValue
             }
-        }
-
-        case "INCREMENT-VALUE": {
+        case "INCREMENT-VALUE":
             return {
                 ...state,
                 num: state.num + 1
             }
-        }
-
-        case "RESET-VALUE": {
+        case "RESET-VALUE":
             return {
                 ...state, num: state.minValue
             }
-        }
-
-        case "ADD-ERROR":{
-
+        case "ADD-ERROR":
             return {
                 ...state, error: action.error
             }
-        }
         default:
             return state
     }
-
-
 }
 
-export const changeMaxValueActionCreator = (maxValue: number) => {
-    return {
-        type: "SET-MAX-VALUE",
-        maxValue
-    } as const
-}
-export const changeMinValueActionCreator = (minValue: number) => {
-    return {
-        type: "SET-MIN-VALUE",
-        minValue
-    } as const
-}
-export const incrementValueActionCreator = () => {
-    return {
-        type: "INCREMENT-VALUE",
-    } as const
-}
-export const resetValueActionCreator = () => {
-    return {
-        type: "RESET-VALUE"
-    } as const
-}
-export const addErrorActionCreator = (error: boolean) => {
-    return {
-        type: "ADD-ERROR",
-        error
-    } as const
-}
+export const changeMaxValueActionCreator = (maxValue: number) => ({type: "SET-MAX-VALUE", maxValue} as const)
+export const changeMinValueActionCreator = (minValue: number) => ({type: "SET-MIN-VALUE", minValue} as const)
+export const incrementValueActionCreator = () => ({type: "INCREMENT-VALUE"} as const)
+export const resetValueActionCreator = () => ({type: "RESET-VALUE"} as const)
+export const addErrorActionCreator = (error: boolean) => ({type: "ADD-ERROR", error} as const)
